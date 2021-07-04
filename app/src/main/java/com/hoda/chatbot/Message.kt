74b877c2260b53aa098c0e7d500009a2d7972683
Hoda-Osama <h.o.elkhodary@student.aast.edu>
2021-07-04 17:11:32 +0200

    your message

---
+++ app/src/main/java/com/hoda/chatbot/Message.kt
@@ -0,0 +1,3 @@
+package com.hoda.chatbot
+
+data class Message(val message: String, val id: String, val time: String)
---
+++ app/src/main/java/com/hoda/chatbot/MessagingAdapter.kt
@@ -0,0 +1,68 @@
+package com.hoda.chatbot
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hoda.chatbot.R
+import com.hoda.chatbot.Message
+import com.hoda.chatbot.Constants.RECEIVE_ID
+import com.hoda.chatbot.Constants.SEND_ID
+import kotlinx.android.synthetic.main.message_item.view.*
+
+class MessagingAdapter: RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
+
+    var messagesList = mutableListOf<Message>()
+
+    inner class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {
+
+                //Remove message on the item clicked
+                messagesList.removeAt(adapterPosition)
+                notifyItemRemoved(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        return MessageViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return messagesList.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val currentMessage = messagesList[position]
+
+        when (currentMessage.id) {
+            SEND_ID -> {
+                holder.itemView.userMessage.apply {
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.botMessage.visibility = View.GONE
+            }
+            RECEIVE_ID -> {
+                holder.itemView.botMessage.apply {
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.userMessage.visibility = View.GONE
+            }
+        }
+    }
+
+    fun insertMessage(message: Message) {
+        this.messagesList.add(message)
+        notifyItemInserted(messagesList.size)
+    }
+
+}
